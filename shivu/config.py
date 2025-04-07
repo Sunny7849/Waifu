de90d4cc58574f0794dbea6c5c0420b7848bc9df
Sunny7849 <164983493+Sunny7849@users.noreply.github.com>
2025-04-08 04:47:33 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -1,24 +1,28 @@
 class Config(object):
     LOGGER = True
-
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
-
+    OWNER_ID = "8156600797"
+    sudo_users = ("7349660966", "8156600797")  # Tuple me proper format
+    GROUP_ID = -1002372914024
+    TOKEN = "7539465396:AAFT5I6oK0wRJHSFNaAUMosQ4uFm2pHa7_c"
     
+    mongo_url = "mongodb+srv://arzuquine:TGDARK11798@madarauzumaki.22wjzeb.mongodb.net/?retryWrites=true&w=majority"
+
+    PHOTO_URL = [
+        "https://graph.org/file/90344336f0da2961141a8-9129c1a27bb0bf675f.jpg",
+        "https://graph.org/file/abc123example.jpg"
+    ]
+
+    SUPPORT_CHAT = "https://t.me/Anime_Circle_Club"
+    UPDATE_CHAT = "https://t.me/+vDcCB_w1fxw1YTll"
+    BOT_USERNAME = "Daddy_Madara_WaifuBot"
+    CHARA_CHANNEL_ID = "-1002674487477"  # <-- yeh updated GC ID hai
+
+    api_id = 28159105
+    api_hash = "a0936ddf210a7e091e19947c7dc70c91"
+
 class Production(Config):
     LOGGER = True
-
 
 class Development(Config):
     LOGGER = True
